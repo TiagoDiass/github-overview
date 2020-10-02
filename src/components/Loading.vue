b289a1ae924afabb632e34ad975f763e0ea7cc6f
TiagoDiass <tiago.costadiasss@gmail.com>
2020-10-02 13:35:15 -0300

    created loading component

---
+++ src/components/Loading.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="loading-animation">
+    <h1>
+      <i class="fas fa-spinner"></i>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/variables';
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading-animation {
+  background-color: $white-darker;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: $primary;
+    display: inline-block;
+    font-size: 3rem;
+    animation: rotate 1.3s linear infinite;
+  }
+}
+</style>
